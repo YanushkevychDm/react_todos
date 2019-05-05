d9f5179a030e238369009bf34cd49ef5685df009
Dmytro Yanushkevych <onswarm@gmail.com>
2019-05-05 19:32:16 +0300

    test btn 2

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React, {Component} from 'react';
 import AddTodos from './addTodos';
 import TodoList from './todoList';
 
+const INITIAL_STATE = {
+  value: {id: 6, task: 'test test'},
+  todos: [
+    {id: 1, task: 'Create my first React app'},
+    {id: 2, task: 'Rendering Arrays in React'},
+    {id: 3, task: 'Manage React State with Arrays'},
+    {id: 4, task: 'Добавить новую задачу из приложения'},
+  ]
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      value: {id: 6, task: 'test tsest'},
-      todos: [
-        {id: 1, task: 'Create my first React app'},
-        {id: 2, task: 'Rendering Arrays in React'},
-        {id: 3, task: 'Manage React State with Arrays'},
-        {id: 4, task: 'Добавить новую задачу из приложения'},
-      ]
-    };
+    this.state = INITIAL_STATE;
   }
 
+
+  onInitialState = () => {
+    this.setState({ ...INITIAL_STATE });
+  };
 
   onClearAll = () => {
     this.setState({todos: [] });
@@ -44,6 +50,7 @@ class App extends Component {
         <div className="test-wrap">
           <button onClick={this.onAddItem} className="test-btn">test test</button>
           <button onClick={this.onClearAll} className="test-btn">Очистить</button>
+          <button onClick={this.onInitialState} className="test-btn">Reset</button>
         </div>
       </div>
     );
