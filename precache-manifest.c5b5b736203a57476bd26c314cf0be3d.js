abde0bd35e975c08c29b8f7a09e015a627cbbdfa
Dmytro Yanushkevych <onswarm@gmail.com>
2019-05-25 15:54:37 +0300

    Updates

---
+++ precache-manifest.c5b5b736203a57476bd26c314cf0be3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2fec2286cca7deccbd9422cf7e31b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acdbef575f240cea145f",
+    "url": "/static/css/main.7ac0d717.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "acdbef575f240cea145f",
+    "url": "/static/js/main.2f35e38e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7369f71e403c80c84bfb94d2dd07ee68",
+    "url": "/static/media/background.7369f71e.jpg"
+  }
+]);
